21dc43eaee16b67711fca258e4e793c71870027e
Adrian <adriancubillos@gmail.com>
2022-03-02 12:06:45 -0500

    Changed CardList componet from function to class

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { CardList } from "./components/card-list/card-list.component";
+import CardList from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
@@ -27,21 +27,27 @@ class App extends Component {
       );
   }
 
-  handleChange = (e) => {
-    this.setState({ searchFiled: e.target.value });
+  onSearchChange = (e) => {
+    this.setState(() => {
+      return { searchFiled: e.target.value.toLowerCase() };
+    });
   };
 
   render() {
     console.log("render");
+
     const { monsters, searchFiled } = this.state;
+    const { onSearchChange } = this;
+
     const filteredMonsters = monsters.filter((monster) =>
-      monster.name.toLowerCase().includes(searchFiled.toLowerCase())
+      monster.name.toLowerCase().includes(searchFiled)
     );
+
     return (
       <div className="App">
         <SearchBox
           placeHolder="Search Monsters"
-          handelChange={this.handleChange}
+          handelChange={onSearchChange}
         ></SearchBox>
         <CardList monsters={filteredMonsters}></CardList>
       </div>
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card } from "../card/card.component";
 import "./card-list.styles.css";
 
-export const CardList = (props) => {
-  return (
-    <div className="card-list">
-      {props.monsters.map((monster) => (
-        <Card key={monster.id} monster={monster}></Card>
-      ))}
-    </div>
-  );
-};
+class CardList extends Component {
+  render() {
+    console.log("render from CardList Component");
+    const { monsters } = this.props;
+
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => (
+          <Card key={monster.id} monster={monster}></Card>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default CardList;
